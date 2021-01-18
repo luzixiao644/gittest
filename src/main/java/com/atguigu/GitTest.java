9237058dc5fddb846e9ef455c7635cd3bb3b0f3a
鲁子啸 <1471003238@qq.com>
2021-01-18 21:27:27 +0800

    更新3

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("22222222222222222222222");
         System.out.println("88888888888888888888888888888888888");
         System.out.println("000000000");
+        System.out.println("6666666");
     }
 }
